9ced129b93b1a232f4cd4ea2826e68e7a02dc50e
Dmitry Sedov <dmsedov92@gmail.com>
2017-09-29 20:42:26 +0300

    add socket to app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+import Express from 'express';
+import bodyParser from 'body-parser';
+import Router from 'named-routes';
+import methodOverride from 'method-override';
+import socketIO from 'socket.io';
+
+export default (port) => {
+  const app = Express();
+  const router = new Router();
+  router.extendExpress(app);
+  router.registerAppHelpers(app);
+  app.set('view engine', 'pug');
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(methodOverride('_method'));
+
+  app.get('/', 'root', (req, res) => {
+    res.render('index');
+  });
+  const io = socketIO.listen(app.listen(port));
+  io.on('connection', (socket) => {
+    console.log('connected successfully');
+    socket.emit('greetings message', { message: 'welcome to the chat' });
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
+};
---
+++ bin/server.js
@@ -0,0 +1,4 @@
+import server from '../app';
+
+const port = 8500;
+server(port);
