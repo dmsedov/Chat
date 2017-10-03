da82f624fc6b86b5cb17a471db005debc7aa0e6a
Dmitry Sedov <dmsedov92@gmail.com>
2017-10-03 14:31:47 +0300

    refactor authentication.js

--- public/authentication.js
+++ public/authentication.js
@@ -1,4 +1,5 @@
 const divCenter = document.getElementsByClassName('center')[0];
+const authForm = document.getElementById('auth-form');
 const nickname = document.getElementById('nickname');
 const password = document.getElementById('password');
 const signInButton = document.getElementById('sign-in');
@@ -10,71 +11,38 @@ const isValidData = (str, regExp) => {
   return result;
 };
 
+const regExpOfNickname = /^[a-zA-Z]{1}[a-zA-Z0-9]{5,11}$/;
+const regExpOfPassword = /^[a-zA-Z0-9]{6,12}$/;
 
 nickname.onfocus = () => {
   const el = document.getElementsByClassName('notification')[0];
   if (el) {
     divCenter.removeChild(el);
-    signInButton.removeAttribute('disabled');
-    signUpButton.removeAttribute('disabled');
   }
 };
 
-nickname.onblur = () => {
-  const str = nickname.value;
-  let note;
-  const regExp = /^[a-zA-Z]{1}[a-zA-Z0-9]{5,11}$/;
-  const el = document.getElementsByClassName('notification')[0];
-  if (str.length !== 0 && !isValidData(str, regExp)) {
-    if (!el) {
-      note = document.createElement('p');
-      note.innerHTML = 'Invalid nickname';
-      note.className = 'notification';
-      divCenter.insertBefore(note, signInButton);
-      signInButton.setAttribute('disabled', 'disabled');
-      signUpButton.setAttribute('disabled', 'disabled');
-    }
-  }
-};
 
 password.onfocus = () => {
   const el = document.getElementsByClassName('notification')[0];
   if (el) {
     divCenter.removeChild(el);
-    signInButton.removeAttribute('disabled');
-    signUpButton.removeAttribute('disabled');
   }
 };
 
-password.onblur = () => {
-  const str = password.value;
+
+authForm.onsubmit = () => {
   const el = document.getElementsByClassName('notification')[0];
-  let note;
-  const regExp = /^[a-zA-Z0-9]{6,12}$/;
-  if (str.length !== 0 && !isValidData(str, regExp)) {
+  const nick = nickname.value;
+  const key = password.value;
+  const cond = isValidData(nick, regExpOfNickname) && isValidData(key, regExpOfPassword);
+  if (!cond) {
     if (!el) {
-      note = document.createElement('p');
-      note.innerHTML = 'Invalid password';
+      const note = document.createElement('p');
+      note.innerHTML = 'Fields nickname and password must be correct or full filled!';
       note.className = 'notification';
       divCenter.insertBefore(note, signInButton);
-      signInButton.setAttribute('disabled', 'disabled');
-      signUpButton.setAttribute('disabled', 'disabled');
     }
+    return false;
   }
+  return true;
 };
-
-  sendButton.onclick = () => {
-    const elements = document.getElementsByClassName('alert-message');
-    if (nickname.value === '') {
-      const message = 'Nickname field must be filled!'
-      if (elements.length === 0) {
-        const alertMessage = document.createElement('p');
-        alertMessage.innerHTML = message;
-        alertMessage.setAttribute('class', 'alert-message');
-        divNicknameField.appendChild(alertMessage);
-      }
-    } else {
-      const text = textarea.value;
-      socket.emit('message', { message: text });
-    }
-  };
