c83fafad204be3519754563e1a7d750be078d555
Dmitry Sedov <dmsedov92@gmail.com>
2017-10-03 19:37:34 +0300

    add .onkeypress and modify .onfocus handlers

--- public/js/authentication.js
+++ public/js/authentication.js
@@ -13,32 +13,48 @@ const isValidData = (str, regExp) => {
 const regExpOfNickname = /^[a-zA-Z]{1}[a-zA-Z0-9]{3,11}$/;
 const regExpOfPassword = /^[a-zA-Z0-9]{6,12}$/;
 
+authForm.onkeypress = (event) => {
+  console.log(event);
+  if (event.keyCode === 13) {
+    event.preventDefault();
+  }
+};
+
 nickname.onfocus = () => {
-  const el = document.getElementsByClassName('notification')[0];
-  if (el) {
-    divCenter.removeChild(el);
+  const pNotification = document.getElementsByClassName('notification')[0];
+  const pErrorMessage = document.getElementsByClassName('error-message')[0];
+  if (pNotification) {
+    divCenter.removeChild(pNotification);
   }
   if (nickname.value === 'must be unique') {
     nickname.value = '';
+  }
+  if (pErrorMessage) {
+    divCenter.removeChild(pErrorMessage);
   }
 };
 
 
 password.onfocus = () => {
-  const el = document.getElementsByClassName('notification')[0];
-  if (el) {
-    divCenter.removeChild(el);
+  const pNotification = document.getElementsByClassName('notification')[0];
+  const pErrorMessage = document.getElementsByClassName('error-message')[0];
+  if (pNotification) {
+    divCenter.removeChild(pNotification);
+  }
+  if (pErrorMessage) {
+    divCenter.removeChild(pErrorMessage);
   }
 };
 
 
 authForm.onsubmit = () => {
-  const el = document.getElementsByClassName('notification')[0];
+  const pNotification = document.getElementsByClassName('notification')[0];
   const nick = nickname.value;
   const key = password.value;
   const cond = isValidData(nick, regExpOfNickname) && isValidData(key, regExpOfPassword);
+  console.log(cond);
   if (!cond) {
-    if (!el) {
+    if (!pNotification) {
       const note = document.createElement('p');
       note.innerHTML = 'Fields nickname and password must be correct or full filled!';
       note.className = 'notification';
